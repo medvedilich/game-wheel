df97a6f966689767439aa77542bdce3091e50c1b
Ivan <87783500+medvedilich@users.noreply.github.com>
2022-08-27 08:44:42 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,21 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="йоу" />
+        <Header id="data" title="йоу" />
         <a href="phone.html">текст ссылки</a>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
+    
 
       <Footer />
     </div>
+    <script>
+    let text = document.GetElementById("data");
+      while(true){
+      text.innerText = event.accelerationIncludingGravity.x;
+    }
+    </script>
   )
 }
